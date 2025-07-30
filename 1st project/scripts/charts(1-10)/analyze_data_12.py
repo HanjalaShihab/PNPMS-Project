1e77288c39f522855cf080b16353eff38e068adb
hanjala <shihab2305341402@diu.edu.bd>
2025-07-30 19:52:48 +0600

    top 10 deaths bar plot

--- 1st project/scripts/charts(1-10)/analyze_data_12.py
+++ 1st project/scripts/charts(1-10)/analyze_data_12.py
@@ -4,3 +4,23 @@ import matplotlib.pyplot as plt
 
 covid_data = pd.read_csv('1st project/data/country_wise_latest.csv')
 
+print(covid_data.head())
+
+#plot a bar chart of top 10 countries with the most deaths:
+x = covid_data.sort_values(by= 'Deaths', ascending= False).head(10)
+print(x)
+
+
+plt.figure(figsize=(10,8))
+plt.title('Top 10 countries with most deaths')
+
+sns.barplot(x = 'Country/Region', y= 'Deaths', data= x)
+
+plt.xlabel('Country')
+plt.ylabel('Deaths')
+
+plt.tight_layout()
+
+plt.savefig('1st project/output/charts/top10_death_cases.png')
+
+plt.show()
